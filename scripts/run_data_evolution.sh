3ae7cc836bd34080ef40bed3f4ae1f59ac6a2fe1
Yougang Lyu <82445958+youganglyu@users.noreply.github.com>
2025-07-26 20:06:17 +0200

    Update run_data_evolution.sh

--- scripts/run_data_evolution.sh
+++ scripts/run_data_evolution.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
 echo "Running query_evolution.py..."
-#python ./src/data_process/query_evolution.py
+python ./src/data_process/query_evolution.py
 
 echo "Running break_subquery.py..."
-#python ./src/data_process/break_subquery.py
+python ./src/data_process/break_subquery.py
 
 echo "Running filter_data.py..."
 python ./src/data_process/filter_data.py
